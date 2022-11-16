5850a78cdb25dcaa9e4b6e186d6e49ecddf73ca9
NashTorres <113381172+NashTorres@users.noreply.github.com>
2022-11-16 13:47:43 -0500

    Section 2 Practice

--- ProblemSet2_2.java
+++ ProblemSet2_2.java
@@ -2,13 +2,14 @@ public class ProblemSet2_2 {
     public static void main(String[] args) {
         //Use print statements to create your own beautiful original ASCII art.
         //Use comments to describe what your image is depicting.
-        
-        
-        
-        
-        
-        
-        
+        System.out.println("    /\\     /\\   ");
+        System.out.println("   /  \\___/  \\  ");
+        System.out.println("  /           \\ ");
+        System.out.println(" {  /\\    /\\   } ");
+        System.out.println("====   V   ======  ");
+        System.out.println(" ====(_|_)======  ");
+        System.out.println("  {            } ");
+        System.out.println("   \\__________/ ");
         
     } 
 }
